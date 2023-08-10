b619ec691a53571e973113d10ca8bbfc2f8a1380
MahinMuhammad <amimahinurrahman@gmail.com>
2023-08-10 20:08:34 +0600

    Initial Commit

---
+++ Poke/PokeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeApp.swift
+//  Poke
+//
+//  Created by Md. Mahinur Rahman on 8/10/23.
+//
+
+import SwiftUI
+
+@main
+struct PokeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
