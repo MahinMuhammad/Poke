c87b22db06cc2fdec11561b9f366f6b538009fcf
MahinMuhammad <amimahinurrahman@gmail.com>
2023-08-11 20:29:36 +0600

    firebase integrated

--- Poke/PokeApp.swift
+++ Poke/PokeApp.swift
@@ -6,10 +6,16 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct PokeApp: App {
     @ObservedObject var router = Router()
+    
+    init(){
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             NavigationStack(path: $router.path){
