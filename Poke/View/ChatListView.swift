79e887050846b668f70fc6d81e376c09b14d2a27
MahinMuhammad <amimahinurrahman@gmail.com>
2023-09-05 19:53:33 +0600

    Chatlist heading + new chatbutton + Searchbar

--- Poke/View/ChatListView.swift
+++ Poke/View/ChatListView.swift
@@ -10,72 +10,111 @@ import SwiftUI
 struct ChatListView: View {
     @StateObject var viewModel = ChatListViewModel()
     @StateObject var d = Dummy()
+    @State var searchBarInput = ""
     var body: some View {
         ZStack {
             Color(K.Colors.canvasColor)
                 .edgesIgnoringSafeArea(.all)
-            List(viewModel.chatCollection){ chat in
-                NavigationLink(value: chat) {
-                    HStack(alignment: .top){
-                        AsyncImage(url: d.propic){  image in
-                            image
-                                .resizable()
-                                .scaledToFill()
-                                .frame(width: 60, height: 60)
-                                .clipped()
-                                .clipShape(Circle())
-                        }placeholder: {
-                            Image(systemName: "photo")
-                                .resizable()
-                                .scaledToFill()
-                                .frame(width: 60, height: 60)
-                                .clipped()
-                                .clipShape(Circle())
-                            
+            VStack {
+                HStack {
+                    Text("Messages")
+                        .fontWeight(.bold)
+                        .font(.system(size: 32))
+                    
+                    Spacer()
+                    
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "square.and.pencil")
+                            .foregroundColor(Color.gray)
+                            .imageScale(.large)
+                    }
+
+                }
+                .padding(.leading,20)
+                .padding(.trailing,20)
+                .padding(.top, 40)
+                
+                RoundedRectangle(cornerRadius: 18)
+                    .foregroundColor(Color(K.Colors.tileColor))
+                    .frame(height: 40)
+                    .padding(.all)
+                    .overlay {
+                        HStack{
+                            Image(systemName: "magnifyingglass")
+                                .foregroundColor(.gray)
+
+                            TextField("Search for messages or users", text: $searchBarInput)
+                                .tint(Color(UIColor.label))
+                                .padding(.all)
                         }
-                        
-                        Spacer()
-                            .frame(width: 20)
-                        
-                        VStack(alignment: .leading){
-                            Text(d.name)
-                                .fontWeight(.bold)
-                                .font(.system(size: 20))
+                        .padding(.leading, 40)
+                    }
+                
+                List(viewModel.chatCollection){ chat in
+                    NavigationLink(value: chat) {
+                        HStack(alignment: .top){
+                            AsyncImage(url: d.propic){  image in
+                                image
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(width: 60, height: 60)
+                                    .clipped()
+                                    .clipShape(Circle())
+                            }placeholder: {
+                                Image(systemName: "photo")
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(width: 60, height: 60)
+                                    .clipped()
+                                    .clipShape(Circle())
+                                
+                            }
                             
                             Spacer()
-                                .frame(height: 10)
+                                .frame(width: 20)
                             
-                            Text(chat.content)
-                                .lineLimit(3)
-                                .fontWeight(.regular)
-                                .font(.system(size: 17))
-                        }
-                        
-                        Spacer()
-                        
-                        VStack{
-                            Text(d.time)
-                                .foregroundColor(Color.gray)
-                                .fontWeight(.thin)
+                            VStack(alignment: .leading){
+                                Text(d.name)
+                                    .fontWeight(.bold)
+                                    .font(.system(size: 20))
+                                
+                                Spacer()
+                                    .frame(height: 10)
+                                
+                                Text(chat.content)
+                                    .lineLimit(3)
+                                    .fontWeight(.regular)
+                                    .font(.system(size: 17))
+                            }
                             
-                            RoundedRectangle(cornerRadius: 6)
-                                .foregroundColor(Color(K.Colors.primaryColor))
-                                .frame(width: 20, height: 20)
-                                .overlay{
-                                    Text(d.numOfMessages)
-                                        .foregroundColor(Color.white)
-                                        .fontWeight(.bold)
-                                }
+                            Spacer()
+                            
+                            VStack{
+                                Text(d.time)
+                                    .foregroundColor(Color.gray)
+                                    .fontWeight(.thin)
+                                
+                                RoundedRectangle(cornerRadius: 6)
+                                    .foregroundColor(Color(K.Colors.primaryColor))
+                                    .frame(width: 20, height: 20)
+                                    .overlay{
+                                        Text(d.numOfMessages)
+                                            .foregroundColor(Color.white)
+                                            .fontWeight(.bold)
+                                    }
+                            }
                         }
                     }
+                    .listRowSeparator(.hidden)
+                    .listRowBackground(Color(K.Colors.canvasColor))
+                    .padding(.bottom, 25)
                 }
-                .listRowSeparator(.hidden)
-                .listRowBackground(Color(K.Colors.canvasColor))
+                .listStyle(.plain)
+                .navigationDestination(for: Chat.self) { chat in
+                    ChatBoxView(sending: chat.senderEmail, receiving: chat.receiverEmail)
             }
-            .listStyle(.plain)
-            .navigationTitle("Messages")
-            .navigationDestination(for: Chat.self) { chat in
-                ChatBoxView(sending: chat.senderEmail, receiving: chat.receiverEmail)
             }
         }
     }
